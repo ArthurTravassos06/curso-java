86065ef0c90737729722e438dd961a3a9f5dbc86
ArthurTravassos06 <arthur.travassos.1094@gmail.com>
2022-06-29 13:30:54 -0300

    Hello world

--- java-sintaxe-exercicio/src/ContaTerminal.java
+++ java-sintaxe-exercicio/src/ContaTerminal.java
@@ -1,6 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.print("Hello Wordl!");
+    public static void main(String[] args) throws Expection {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência : \n");
+        int numero = scanner.nextInt();
+        
     }
     
 }
