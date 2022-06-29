e28b976ea4941cb7b15f9526d354f81fdc651946
ArthurTravassos06 <arthur.travassos.1094@gmail.com>
2022-06-29 14:45:06 -0300

    exercicio sintaxe

--- java-sintaxe-exercicio/src/ContaTerminal.java
+++ java-sintaxe-exercicio/src/ContaTerminal.java
@@ -2,13 +2,26 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Expection {
-        
+    public static void main(String[] args) {
+    
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Por favor, digite o número da Agência : \n");
+        System.out.println("Por favor, digite o número da Conta :");
         int numero = scanner.nextInt();
         
+        System.out.println("Por favor, digite o número da Agência :");
+        String agencia = scanner.next();
+
+        scanner.nextLine();
+        System.out.println("Por favor, digite o seu nome completo :");
+        String nomeCompleto = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo incial :");
+        double saldo = scanner.nextDouble();
+        
+        
+        
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.", nomeCompleto, agencia, numero, saldo);
     }
     
 }
