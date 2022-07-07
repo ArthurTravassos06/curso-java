c98205c22cf480f5d9d7d56f65d730da2a67a5b0
ArthurTravassos06 <arthur.travassos.1094@gmail.com>
2022-07-07 20:32:25 -0300

    exercicio uml commit

--- java-poo-uml-exercicio/src/App.java
+++ java-poo-uml-exercicio/src/App.java
@@ -1,5 +1,19 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Iphone iphone = new Iphone();
+
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarPasta();
+
+        iphone.exibirPagina();
+        iphone.atualizarPagina();
+        iphone.abrirNovaAba();
     }
 }
---
+++ java-poo-uml-exercicio/src/Iphone.java
@@ -0,0 +1,34 @@
+
+public class Iphone implements AparelhoTelefonico,NavegadorInternet,ReprodutorMusical {
+    public void ligar(){
+        System.out.println("Ligando");
+    };
+    public void atender(){
+        System.out.println("Atendendo");
+    };
+    public void iniciarCorreioVoz(){
+        System.out.println("Iniciando Correio de Voz");
+    };
+
+    public void exibirPagina(){
+        System.out.println("Exibindo a página");
+    };
+    public void atualizarPagina(){
+        System.out.println("Atualizando a página");
+    };
+    public void abrirNovaAba(){
+        System.out.println("Abrindo uma nova aba");
+    };
+
+    public void tocar(){
+        System.out.println("Tocando música");
+    };
+    public void pausar(){
+        System.out.println("pausa a música");
+    };
+    public void selecionarPasta(){
+        System.out.println("Selecionando a pasta");
+    };
+   
+    
+}
